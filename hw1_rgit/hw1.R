4641b9378a47bbbbdab702be6cdd7205ff19fa77
Yuxi Wu <yuxiwu@uchicago.edu>
2017-10-04 18:26:04 -0500

    beginnings of hw1

---
+++ hw1_rgit/hw1.R
@@ -0,0 +1,10 @@
+# Yuxi Wu
+# Exploratory data analysis of DoT FARS data.
+
+install.packages(c("readr","haven","dplyr","tidyr","stringr","ggplot2"))
+library("readr")
+library("haven")
+library("dplyr")
+library("tidyr")
+library("stringr")
+library("ggplot2")
