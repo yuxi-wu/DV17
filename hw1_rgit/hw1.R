842fcc4d8ed1bda8e97fef957d6172e40a300c6e
Yuxi Wu <yuxiwu@uchicago.edu>
2017-10-04 19:36:52 -0500

    combining two

--- hw1_rgit/hw1.R
+++ hw1_rgit/hw1.R
@@ -14,3 +14,22 @@ acc2015 <- read_csv("accident.csv")
 acc2014 <- read_sas("accident.sas7bdat")
 class(acc2014)
 class(acc2015)
+
+acc2014 <- mutate(acc2014, TWAY_ID2=na_if(acc2014$TWAY_ID2, ""))
+table(is.na(acc2014$TWAY_ID2))
+
+dim(acc2014)
+dim(acc2015)
+
+colnames(acc2014) %in% colnames(acc2015)
+colnames(acc2015) %in% colnames(acc2014)
+
+colnames(acc2014)[[19]] 
+# missing from 2015: "ROAD_FNC"
+colnames(acc2015)[c(19,20,21)]
+# missing from 2014 "RUR_URB", "FUNC_SYS", "RD_OWNER"
+
+acc <- bind_rows(acc2014, acc2015)
+count(acc, RUR_URB)
+# RUR_URB was not a column in the 2014 data, which accounts for half of the rows in acc.
+# Thus, half the rows in acc do not have a RUR_URB entry.
